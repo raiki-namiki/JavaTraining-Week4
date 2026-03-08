73586dcef7d51dc4179669cae294e149e020c354
並木来樹 <raiki.namiki@tokyo.supersoftware.co.jp>
2026-03-08 21:23:50 +0900

    Initial commit: Spring Initializrでプロジェクト作成

---
+++ src/main/java/com/example/taskapp/JavaTrainingFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainingFinalApplication.class, args);
+	}
+
+}
