272afb9394cf9528032b84a5000fdaf370acc2ee
並木来樹 <raiki.namiki@tokyo.supersoftware.co.jp>
2026-03-08 23:41:21 +0900

    最終課題：README、スクショを追加

--- src/test/java/com/example/taskapp/JavaTrainingFinalApplicationTests.java
+++ src/test/java/com/example/taskapp/JavaTrainingFinalApplicationTests.java
@@ -1,13 +1,29 @@
-package com.example.taskapp;
+package com.example.taskapp; 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest
 class JavaTrainingFinalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext context;
 
+    @Test
+    void contextLoads() {
+        
+    }
+
+    @Test
+    void testSecurity() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+
+        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/tasks"))
+               .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().is3xxRedirection());
+    }
 }
